9d7a25590a152164187064c4e11186b861c35ecd
almwld <your-email@example.com>
2026-02-18 03:59:09 +0300

    Pure root structure fix

---
+++ lib/main.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(home: Scaffold(appBar: AppBar(title: Text('فليكس يمن 🇾🇪')), body: Center(child: Text('تم الربط بنجاح!')))));
